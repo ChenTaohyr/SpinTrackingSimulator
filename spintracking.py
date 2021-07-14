5b19d9b08869e2489159315d0fb152ff49766511
ChenTao <chentao@ChenTaodeMacBook-Air.local>
2021-07-14 11:50:56 +0800

    first commit

---
+++ spintracking.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3.9
+# -*- coding: UTF-8 -*-
+
+import sys
+
+      
+file_name = sys.argv[1]           #Get input file
+
+f=open(file_name)
+parameters={}
+while True:                      #Read every parameters and save them in dir
+         line=f.readline()
+         line=line.strip()
+         if not line:
+              break
+         else:
+             parameters[line.split('=')[0]] = line.split('=')[1] 
+             
+             
+f.close()   
+    
+print(parameters)
