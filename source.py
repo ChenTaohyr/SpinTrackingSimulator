e8f225b2fedc61eecf15875a4ec682c10fa3def0
‘ct’ <adgrtgs1434144675@qq.com>
2021-08-11 10:48:44 +0800

    complete Completed the main part of the code

--- source.py
+++ source.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 import math
+import cmath
 
 #Initialize of the beam
 def Initialize(InitialPolarizationDegree,ParticleNum):
@@ -38,3 +39,46 @@ def Snake(Spinor):
         Spinor[i][0]=Spinor[i][1]
         Spinor[i][1]=-1*a
     return Spinor
+
+#rotate into resonance precessing frame ,apply the resonance kick then rotate back to particle rest frame
+#the strength of resonance is replaced by a estimated value of 0.002*GGamma 
+def ResonanceKick(Spinor,GGamma,K):
+    eplison=GGamma*0.002
+    delta=K-GGamma
+    lamda=math.sqrt(delta*delta+eplison*eplison)
+    b=(eplison*math.sin(lamda*math.pi))/lamda
+    a=math.sqrt(1-b*b)
+    c=math.atan((delta*math.tan(lamda*math.pi)/lamda))
+    d=0
+    
+    Length=len(Spinor)
+    for i in range(Length):
+        
+        x=Spinor[i][0]
+        Spinor[i][0]=Spinor[i][0]*a*cmath.exp((c-K*math.pi)*1j)+1j*b*Spinor[i][1]*cmath.exp(-1j*(d+K*math.pi))
+        Spinor[i][1]=x*(-1j)*b*cmath.exp(1j*(d+K*math.pi))+Spinor[i][1]*a*cmath.exp(-1j*(c-K*math.pi))
+       
+
+
+    return Spinor
+
+
+
+
+#Calculate polarization degree
+#polarization degree=<<Sigma3>>
+def CalPorDegree(Spinor):
+    sigma=[]
+    Length=len(Spinor)
+    for i in range(Length):
+        sigmai=abs(Spinor[i][0])*abs(Spinor[i][0])-abs(Spinor[i][1])*abs(Spinor[i][1])
+        sigma.append(sigmai)
+    sum=0
+    for i in sigma:
+        sum=sum+i
+        Pordegree=sum/Length
+    print(sigma)
+
+    return Pordegree
+
+
--- spintracking.py
+++ spintracking.py
@@ -3,6 +3,8 @@
 
 import sys
 import source
+import matplotlib.pylab as pyl
+import numpy as np
       
 file_name = sys.argv[1]           #Get input file
 
@@ -23,9 +25,48 @@ print('parameters: ' ,parameters)
 
 Spinor=\
 source.Initialize(float(parameters['InitialPolarizationDegree']),int(parameters['ParticleNum']))
-#print(Spinor)
+
+
+#SpinorAfterArc=source.Arc(Spinor,GGamma)
+#Spinor2=source.Snake(SpinorAfterArc)
+
 G=0.001159652
-GGamma=G*1
-SpinorAfterArc=source.Arc(Spinor,GGamma)
-Spinor2=source.Snake(SpinorAfterArc)
-#print(Spinor2)
+
+#For picture
+PorPlotY=[]
+PorPlotX=[]
+
+#electron energy from 0 to 40GeV,corresponding to Gamma from 1 to 80000
+#Gamma raise 1 per turn
+#Temporarily，only regular imperfection resonances is considered
+#Resonance tune K is selected as the integer nearest GGamma
+
+
+for i in range(300):
+     Gamma=i+100
+     GGamma=G*Gamma
+     IntGGamma=int(GGamma)
+     if (GGamma-IntGGamma>0.5):
+          K=IntGGamma+1
+          SpinorAfterKick=source.ResonanceKick(Spinor,GGamma,K)
+          Spinor=source.Snake(SpinorAfterKick)
+          if(i%100==0):
+               p=source.CalPorDegree(Spinor)
+               PorPlotX.append(GGamma)
+               PorPlotY.append(p)
+     else:
+          K=IntGGamma
+          SpinorAfterKick=source.ResonanceKick(Spinor,GGamma,K)
+          Spinor=source.Snake(SpinorAfterKick)
+          if(i%100==0):
+               p=source.CalPorDegree(Spinor)
+               PorPlotX.append(GGamma)
+               PorPlotY.append(p)
+
+#Draw the picture of the change of porlization degree
+#pyl.plot(PorPlotX, PorPlotY)
+#pyl.show()
+
+
+
+
