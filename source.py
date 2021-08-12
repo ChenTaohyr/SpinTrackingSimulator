b761c78d45f069da6bbf176062f2b8cd743e451d
‘ct’ <adgrtgs1434144675@qq.com>
2021-08-12 16:21:15 +0800

    a more general version

--- source.py
+++ source.py
@@ -32,33 +32,67 @@ def Arc(Spinor,GGamma):
     return Spinor
 
 #A full snake with angle pi,snake axis lies on the beam direction.
-def Snake(Spinor):
-    Length=len(Spinor)
-    for i in range(Length):
-        a=Spinor[i][0]
-        Spinor[i][0]=-1*Spinor[i][1]
-        Spinor[i][1]=a
-    return Spinor
+#def Snake(Spinor):
+ #   Length=len(Spinor)
+  #  for i in range(Length):
+   #     a=Spinor[i][0]
+   #     Spinor[i][0]=-1*Spinor[i][1]
+    #    Spinor[i][1]=a
+   # return Spinor
 
 #A full snake with angle pi,snake axis on transverse radial direction.
-def Snake2(Spinor):
+#def Snake2(Spinor):
+  #  Length=len(Spinor)
+   # for i in range(Length):
+   #     a=Spinor[i][0]
+   #     Spinor[i][0]=-1j*Spinor[i][1]
+   #     Spinor[i][1]=-1j*a
+    #return Spinor
+
+
+#A general snake module
+#the snake axis direction is (cos[phis] ,sin[phis] , 0)
+#phis varies from -pi to pi,phi from 0 to pi.
+def Snake(Spinor,phi,phis):
+    cc=math.cos((phi/2)*math.cos(phis))
+    cs=math.cos((phi/2)*math.sin(phis))
+    ss=math.sin((phi/2)*math.sin(phis))
+    sc=math.sin((phi/2)*math.cos(phis))
+    m11=cc*cs-1j*ss*sc
+    m12=-1*cc*ss-1j*cs*sc
+    m21=cc*ss-1j*cs*sc
+    m22=cc*cs+1j*ss*sc
+
     Length=len(Spinor)
     for i in range(Length):
         a=Spinor[i][0]
-        Spinor[i][0]=-1j*Spinor[i][1]
-        Spinor[i][1]=-1j*a
+        Spinor[i][0]=m11*Spinor[i][0]+m12*Spinor[i][1]
+        Spinor[i][1]=a*m21+m22*Spinor[i][1]
+
     return Spinor
+  
+
+
+
+
+
+
+
+
 
 #rotate into resonance precessing frame ,apply the resonance kick then rotate back to particle rest frame
-#the strength of resonance is replaced by a estimated value of 0.002*GGamma 
-def ResonanceKick(Spinor,GGamma,K):
-    eplison=GGamma*0.002
+####(the strength of resonance is replaced by a estimated value of 0.002*GGamma )deleted####
+#eplison is the strength of resonance ,it is a complex
+#The calculation is based on the Spin Transfer Matrix of constant GGamma ,suitable for all kinds of resonance
+#The resonance kick is applied in a complete turn ie. theta from 0 to 2pi
+def ResonanceKick(Spinor,GGamma,K,eplison):
+    #eplison=GGamma*0.002
     delta=K-GGamma
-    lamda=math.sqrt(delta*delta+eplison*eplison)
-    b=(eplison*math.sin(lamda*math.pi))/lamda
+    lamda=math.sqrt(delta*delta+abs(eplison)*abs(eplison))
+    b=(abs(eplison)*math.sin(lamda*math.pi))/lamda
     a=math.sqrt(1-b*b)
     c=math.atan((delta*math.tan(lamda*math.pi)/lamda))
-    d=0
+    d=-1*cmath.phase(eplison)
     
     Length=len(Spinor)
     for i in range(Length):
--- spintracking.py
+++ spintracking.py
@@ -6,12 +6,13 @@ import source
 import matplotlib.pylab as pyl
 import matplotlib.pyplot as plt
 import numpy as np
+import math
       
 file_name = sys.argv[1]           #Get input file
 
 f=open(file_name)
 parameters={}
-while True:                      #Read every parameters and save them in dir
+while True:                      #Read every parameters and save them in dir 
          line=f.readline()
          line=line.strip()
          if not line:
@@ -24,9 +25,27 @@ f.close()
     
 print('parameters: ' ,parameters)
 
+#initialize the beam
 Spinor=\
 source.Initialize(float(parameters['InitialPolarizationDegree']),int(parameters['ParticleNum']))
 
+#read resonance data
+ResonanceFile=parameters['ResonanceFile']
+#print(ResonanceFile,type(ResonanceFile))
+
+f=open(ResonanceFile)
+ResonanceData={}
+while True:                      
+         line=f.readline()
+         line=line.strip()
+         if not line:
+              break
+         else:
+             ResonanceData[float(line.split(' ')[0])] = float(line.split(' ')[1] )
+             
+             
+f.close() 
+#print(ResonanceData)
 
 #SpinorAfterArc=source.Arc(Spinor,GGamma)
 #Spinor2=source.Snake(SpinorAfterArc)
@@ -34,43 +53,73 @@ source.Initialize(float(parameters['InitialPolarizationDegree']),int(parameters[
 G=0.001159652
 
 #For picture
-PorPlotY=[]
-PorPlotX=[]
+#PorPlotY=[]
+#PorPlotX=[]
 
+
+####deleted####
 #electron energy from 0 to 40GeV,corresponding to Gamma from 1 to 80000
 #Gamma raise 1 per turn
 #Temporarily，only regular imperfection resonances is considered
 #Resonance tune K is selected as the integer nearest GGamma
 
 
-for i in range(8000):
-     Gamma=i+100
-     GGamma=G*Gamma
-     IntGGamma=int(GGamma)
-     if (GGamma-IntGGamma>0.5):
-          K=IntGGamma+1
-          SpinorAfterKick=source.ResonanceKick(Spinor,GGamma,K)
-          Spinor=source.Snake2(SpinorAfterKick)
-          if(i%100==1):
-               p=source.CalPorDegree(Spinor)
-               #p=source.CalPorDegree(SpinorAfterKick)
-               PorPlotX.append(GGamma)
-               PorPlotY.append(p)
-     else:
-          K=IntGGamma
-          SpinorAfterKick=source.ResonanceKick(Spinor,GGamma,K)
-          Spinor=source.Snake2(SpinorAfterKick)
-          if(i%100==1):
-               p=source.CalPorDegree(Spinor)
-               #p=source.CalPorDegree(SpinorAfterKick)
-               PorPlotX.append(GGamma)
-               PorPlotY.append(p)
+#for i in range(1):
+#     Gamma=i+100
+ #    GGamma=G*Gamma
+ #   IntGGamma=int(GGamma)
+  #   if (GGamma-IntGGamma>0.5):
+   #       K=IntGGamma+1
+    #      SpinorAfterKick=source.ResonanceKick(Spinor,GGamma,K,0)
+     #     Spinor=source.Snake(SpinorAfterKick,pi,0)
+      #    if(i%100==1):
+       #        p=source.CalPorDegree(Spinor)
+        #       #p=source.CalPorDegree(SpinorAfterKick)
+         #      PorPlotX.append(GGamma)
+          #     PorPlotY.append(p)
+     #else:
+     #     K=IntGGamma
+     #    SpinorAfterKick=source.ResonanceKick(Spinor,GGamma,K,0)
+     #     Spinor=source.Snake(SpinorAfterKick,pi,0)
+     #     if(i%100==1):
+     #          p=source.CalPorDegree(Spinor)
+     #          #p=source.CalPorDegree(SpinorAfterKick)
+     #          PorPlotX.append(GGamma)
+     #          PorPlotY.append(p)
+#####
+
+
+#Implement the acceleration
+#Temporarily, only resonance that close to GGamma is considered
+InitialGamma=int(parameters['InitialGamma'])
+FinalGamma=int(parameters['FinalGamma'])
+EnergyRisePerTurn=int(parameters['EnergyRisePerTurn'])
+Gamma=InitialGamma
+#ResonanceNum=len(ResonanceData)
+#print(ResonanceNum)
+while (Gamma<FinalGamma):
+          GGamma=G*Gamma
+
+          for i in ResonanceData:
+               if(abs(GGamma-i)<=0.5 ):
+                    Spinor=source.ResonanceKick(Spinor,GGamma,i,complex(GGamma*ResonanceData[i]))
+
+          if (parameters['UseSnake']=='1'):
+               Spinor=source.Snake(Spinor,float(parameters['phi']),float(parameters['phis']))
+          Gamma=Gamma+EnergyRisePerTurn
+
+p=source.CalPorDegree(Spinor)
+print(p)
+
+
+
+
 
 #Draw the picture of the change of porlization degree
 #print(PorPlotX)
 #print(PorPlotY)
-plt.scatter(PorPlotX, PorPlotY)
-plt.show()
+#plt.scatter(PorPlotX, PorPlotY)
+#plt.show()
 
 
 
